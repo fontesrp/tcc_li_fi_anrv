49ac9d3a64ae89fc9279321baede63ac38940774
fontesrp <fontesr2@gmail.com>
2016-08-02 22:14:34 -0300

    Add 8B10B encoding and pin_comm functions to be used in the main program.

--- Software/tcc_li_fi/tcc_li_fi.h
+++ Software/tcc_li_fi/tcc_li_fi.h
@@ -1,4 +1,14 @@
 #ifndef tcc_li_fi_h
 #define tcc_li_fi_h
 
+// encoding_8b10b.ino
+void setup8B10B();
+unsigned int encode8B10B(unsigned char data);
+unsigned char decode8B10B(unsigned int data);
+
+// pin_comm.ino
+void setupPinComm();
+unsigned int receive10b();
+void send10b(unsigned int data);
+
 #endif
