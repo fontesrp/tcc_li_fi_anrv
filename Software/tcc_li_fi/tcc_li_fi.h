caf8417bc285ece8c77d970f08868f47c58bc7f4
fontesrp <fontesr2@gmail.com>
2016-08-05 22:30:12 -0300

    Add basic messaging
    Used the encoding and communication functions to create a basic
    messaging system between two Arduinos.

--- Software/tcc_li_fi/tcc_li_fi.h
+++ Software/tcc_li_fi/tcc_li_fi.h
@@ -1,14 +1,36 @@
 #ifndef tcc_li_fi_h
 #define tcc_li_fi_h
 
-// encoding_8b10b.ino
+// -- encoding_8b10b.ino
 void setup8B10B();
 unsigned int encode8B10B(unsigned char data);
 unsigned char decode8B10B(unsigned int data);
+// --
 
-// pin_comm.ino
+// -- pin_comm.ino
+
+// Pointer to the 8b10b encoding function
+#ifndef ENCODE_FUNCTION
+#define ENCODE_FUNCTION
+typedef unsigned int (*encodeFunction)(unsigned char data);
+#endif
+
+// Functions
 void setupPinComm();
+void generateSyncMessage(encodeFunction encode);
+void sendSyncMessage();
+void waitSyncMessage();
 unsigned int receive10b();
 void send10b(unsigned int data);
+
+// --
+
+#define MESSAGE_SIZE 21
+
+#if defined(__AVR_ATmega2560__) // Arduino Mega 2560 (sender)
+const unsigned char message[MESSAGE_SIZE] = "Testing Arduino comm";
+#elif defined(__AVR_ATmega328P__) // Arduino UNO R3 (receiver)
+volatile unsigned char message[MESSAGE_SIZE] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+#endif
 
 #endif
--- Software/tcc_li_fi/tcc_li_fi.ino
+++ Software/tcc_li_fi/tcc_li_fi.ino
@@ -1,9 +1,39 @@
 #include "tcc_li_fi.h"
 
 void setup() {
-	// Code here
+	setup8B10B();
+	setupPinComm();
+	generateSyncMessage(encode8B10B);
 }
 
+#if defined(__AVR_ATmega2560__) // Arduino Mega 2560 sending
+
 void loop() {
-	// Code here
+
+	int i;
+	unsigned int encodedLetter;
+
+	sendSyncMessage();
+
+	for (i = 0; i < MESSAGE_SIZE; i++) {
+		encodedLetter = encode8B10B(message[i]);
+		send10b(encodedLetter);
+	}
 }
+
+#elif defined(__AVR_ATmega328P__) // Arduino UNO R3 receiving
+
+void loop() {
+
+	int i;
+	unsigned int encodedLetter;
+
+	waitSyncMessage();
+
+	for (i = 0; i < MESSAGE_SIZE; i++) {
+		encodedLetter = receive10b();
+		message[i] = decode8B10B(encodedLetter);
+	}
+}
+
+#endif
