83b937be391ad6fa3a0b2fa6124be15a710f1ef6
fontesrp <fontesr2@gmail.com>
2016-08-08 00:19:28 -0300

    Enable serial for debugging

--- Software/tcc_li_fi/tcc_li_fi.ino
+++ Software/tcc_li_fi/tcc_li_fi.ino
@@ -1,6 +1,7 @@
 #include "tcc_li_fi.h"
 
 void setup() {
+	Serial.begin(9600);
 	setupPinComm();
 }
 
@@ -8,14 +9,44 @@ void setup() {
 
 void loop() {
 
+	int i;
+
+	Serial.println("main: fired");
+
 	sendPhrase(message, MESSAGE_SIZE);
+
+	Serial.println("main: message sent");
+	Serial.print("main: message = ");
+
+	for (i = 0; i < MESSAGE_SIZE; i++) {
+		Serial.print(message[i]);
+	}
+
+	Serial.println("");
+
+	// delay(10000);
 }
 
 #elif defined(__AVR_ATmega328P__) // Arduino UNO R3 receiving
 
 void loop() {
 
+	int i;
+
+	Serial.println("main: fired");
+
 	receivePhrase(message, MESSAGE_SIZE);
+
+	Serial.println("main: message received");
+	Serial.print("main: message = ");
+
+	for (i = 0; i < MESSAGE_SIZE; i++) {
+		Serial.print(message[i]);
+	}
+
+	Serial.println("");
+
+	delay(10000);
 }
 
 #endif
