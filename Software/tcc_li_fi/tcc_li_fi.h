90c0d778c483233fa66eb4c59e6aff41a0431b7e
fontesrp <fontesr2@gmail.com>
2016-08-28 13:08:48 -0300

    Add comments to make things clearer

--- Software/tcc_li_fi/tcc_li_fi.h
+++ Software/tcc_li_fi/tcc_li_fi.h
@@ -1,19 +1,23 @@
 #ifndef tcc_li_fi_h
 #define tcc_li_fi_h
 
-// -- pin_comm.ino
-void setupPinComm();
-void receivePhrase(unsigned char * message, unsigned char messageSize);
-void sendPhrase(unsigned char * message, unsigned char messageSize);
-// --
-
+// Comm messages have a fixed size
 #define MESSAGE_SIZE 21
+
+// Board's role defined by hardware
 #define ROLE_PIN 24
 #define SENDER HIGH
 #define RECEIVER LOW
 
+// From pin_comm.ino
+void setupPinComm();
+void receivePhrase(unsigned char * message, unsigned char messageSize);
+void sendPhrase(unsigned char * message, unsigned char messageSize);
+
+// Sender or receiver
 char boardRole;
 
+// Comm messages
 unsigned char sendMessage[MESSAGE_SIZE] = "Testing Arduino comm";
 unsigned char receivedMessage[MESSAGE_SIZE] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
 
