3d3fc18e9984d31299419815cf39d9039b993397
fontesrp <fontesr2@gmail.com>
2016-10-16 11:30:22 -0200

    Remove old, commented code

--- Software/tcc_li_fi/tcc_li_fi.ino
+++ Software/tcc_li_fi/tcc_li_fi.ino
@@ -18,41 +18,30 @@ void setup() {
 
 void loop() {
 
-	// int i, check = 0;
-
 	Serial.println("loop: fired");
 
 	if (boardRole == SENDER) {
+
 		Serial.println("loop: boardRole = SENDER");
+
 		enableDisplayControl();
 		translateDisplayMessage(message, MESSAGE_SIZE);
 		sendPhrase(message, MESSAGE_SIZE);
+
 		Serial.println("loop: message sent");
+
 	} else if (boardRole == RECEIVER) {
+
 		Serial.println("loop: boardRole = RECEIVER");
+
 		receivePhrase(message, MESSAGE_SIZE);
 		displayTextMessage(message, MESSAGE_SIZE);
+
 		Serial.println("loop: message received");
+
 	} else {
+
 		Serial.println("[error] loop: invalid boardRole");
+
 	}
-
-	/*Serial.print("loop: message =");
-
-	for (i = 0; i < MESSAGE_SIZE; i++) {
-		Serial.print(" 0x");
-		Serial.print(message[i], HEX);
-		if (message[i] != '\0' && message[i] == outputMessage[i]) {
-			check++;
-		}
-	}
-
-	Serial.println("");
-
-	if (boardRole == RECEIVER) {
-		// delay(10000);
-		Serial.print(check, DEC);
-		Serial.print(" correct letters");
-		Serial.println("");
-	}*/
 }
